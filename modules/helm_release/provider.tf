8a414208dd7839c6642e56b5443bb835087787b5
raj-830 <rajarshimu@google.com>
2026-07-02 21:27:17 +0530

    Update provider.tf

--- modules/helm_release/provider.tf
+++ modules/helm_release/provider.tf
@@ -1,10 +1,20 @@
 # 1. CORRECTED DATA SOURCE: Use "id" instead of "name"
+
+data "google_container_cluster" "primary" {
+  name     = reverse(split("/", var.cluster_id))[0]
+  #name     = var.cluster_id
+  location = var.gcp_location
+  project  = var.gcp_project_id
+}
+
+
+/*
 data "google_container_cluster" "primary" {
   name     = var.cluster_id
   location = var.gcp_zone
   project  = var.gcp_project_id
 }
-
+*/
 # 2. KUBERNETES PROVIDER CONFIGURATION
 provider "kubernetes" {
   host                   = "https://${data.google_container_cluster.primary.control_plane_endpoints_config[0].dns_endpoint_config[0].endpoint}"
